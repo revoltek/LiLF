3a52c7f49deeb0b7b9555dba7123e1fbb003b9cf
Martijn Oei <Martijn@l-145-118-226-212.leidenuniv.nl>
2017-08-24 20:04:25 +0200

    flagging updates

--- lib_ms.py
+++ lib_ms.py
@@ -11,107 +11,116 @@ import numpy as np
 
 class AllMSs(object):
 
-    def __init__(self, MSs, scheduler):
+    def __init__(self, pathsMS, scheduler):
         """
-        MSs : list of MS filenames
-        s   : scheduler obj
+        pathsMS:   list of MS paths
+        scheduler: scheduler object
         """
         self.scheduler    = scheduler
-        
-        self.mss_list_str = sorted(mss)
-        
+
+        self.mss_list_str = sorted(pathsMS)
+
         self.mss_list_obj = []
-        for ms in sorted(mss):
-            self.mss_list_obj.append(Ms(ms))
-    
-    
+        for pathMS in self.mss_list_str:
+            self.mss_list_obj.append(MS(pathMS))
+
+
     def get_list_obj(self):
         return self.mss_list_obj
-    
-    
+
+
     def get_list_str(self):
         return self.mss_list_str
-    
-    
+
+
     def get_str_wsclean(self):
         """
-        Return a string with all mss names,
-        useful for wsclean
+        Return a string with all MS paths, useful for wsclean
         """
         return ' '.join(self.mss_list_str)
-    
-    
-    def run(self, cmd, log, cmd_type):
-        for ms in self.mss_list_str:
-            cmd = cmd.replace("$ms", ms)
-            log = log.replace("$ms", ms)
-            self.s.add(cmd, log, cmd_type)
-            
-        self.s.run(check = True)
 
-    
+
+    def run(self, command, commandType, log):
+        for pathMS in self.mss_list_str:
+            commandCurrent = command.replace("$ms", pathMS)
+            logCurrent     = log.replace("$ms", pathMS)
+            self.scheduler.add(commandCurrent, logCurrent, commandType)
+
+        self.scheduler.run(check = True)
+
+
 class MS(object):
-    
-    def __init__(self, filename):
-        self.ms = filename
-    
-    
+
+    def __init__(self, pathMS):
+        """
+        pathMS:        path to the MS
+        pathDirectory: path to the parent directory of the MS
+        nameMS:        name of the MS, without parent directories and extension
+        """
+        self.pathMS        = pathMS
+
+        indexLastSlash     = self.pathMS.rfind('/')
+
+        self.pathDirectory = self.pathMS[ : indexLastSlash]
+        self.nameMS        = self.pathMS[indexLastSlash + 1 : -3]
+
+
     def getName(self):
         """
         Retrieve source name.
         """
-        pathFieldTable = self.ms + "/FIELD"
+        pathFieldTable = self.pathMS + "/FIELD"
         name           = (tables.taql("select NAME from $pathFieldTable")).getcol("NAME")[0]
         return name
-    
-    
+
+
     def isCalibrator(self):
         """
         Returns whether the source is a calibrator or not.
         """
-        return (self.getName() in ["CygA", "3C48", "3C147", "3C196", "3C286", "3C295", "3C380"]) # Expand this list!
-    
-    
+        return (self.getName() in ["CygA", "3C48", "3C147", "3C196", "3C286", "3C295", "3C380"]) # This list should be expanded to include all potential calibrators.
+
+
     def find_nchan(self):
         """
         Find number of channels
         """
-        with tables.table(self.ms + "/SPECTRAL_WINDOW", ack = False) as t:
+        with tables.table(self.pathMS + "/SPECTRAL_WINDOW", ack = False) as t:
             nchan = t.getcol("NUM_CHAN")
-        assert (nchan[0] == nchan).all() # all spw have same channels?
-        
-        logging.debug("%s: channel number: %i", self.ms, nchan[0])
-        #logger.debug("%s: channel number: %i", self.ms, nchan[0])
+        assert (nchan[0] == nchan).all() # all SpWs have same channels?
+
+        logging.debug("%s: channel number (1): %i", self.pathMS, nchan[0])
+        #logger.debug("%s: channel number (1): %i", self.pathMS, nchan[0])
         return nchan[0]
-    
-    
+
+
     def find_chanband(self):
-        """+
+        """
         Find bandwidth of a channel in Hz
         """
-        with tables.table(self.ms + "/SPECTRAL_WINDOW", ack = False) as t:
+        with tables.table(self.pathMS + "/SPECTRAL_WINDOW", ack = False) as t:
             chan_w = t.getcol("CHAN_WIDTH")[0]
         assert all(x == chan_w[0] for x in chan_w) # all chans have same width
-        
-        logging.debug("%s: channel width (MHz): %f", self.ms, chan_w[0] / 1.e6)
-        #logger.debug("%s: channel width (MHz): %f", self.ms, chan_w[0] / 1.e6)
+
+        logging.debug("%s: channel width (MHz): %f", self.pathMS, chan_w[0] / 1.e6)
+        #logger.debug("%s: channel width (MHz): %f", self.pathMS, chan_w[0] / 1.e6)
         return chan_w[0]
-    
-    
+
+
     def find_timeint(self):
         """
         Get time interval in seconds
         """
-        with tables.table(self.ms, ack = False) as t:
-            Ntimes = len(set(t.getcol('TIME')))
-        with tables.table(self.ms + "/OBSERVATION", ack = False) as t:
-            deltat = (t.getcol('TIME_RANGE')[0][1] - t.getcol('TIME_RANGE')[0][0]) / Ntimes
-        
-        logging.debug("%s: Time interval: %f s", self.ms, deltat)
-        #logger.debug('%s: Time interval: %f s' (self.ms, deltat))
+        with tables.table(self.pathMS, ack = False) as t:
+            nTimes = len(set(t.getcol("TIME")))
+        with tables.table(self.pathMS + "/OBSERVATION", ack = False) as t:
+            deltat = (t.getcol("TIME_RANGE")[0][1] - t.getcol("TIME_RANGE")[0][0]) / nTimes
+
+        logging.debug("%s: time interval (s): %f", self.pathMS, deltat)
+        #logger.debug("%s: time interval (s): %f", self.pathMS, deltat)
         return deltat
-    
-    
+
+
     def get_phase_centre(self):
         """
         Get the phase centre of the first source (is it a problem?) of an MS
@@ -119,40 +128,40 @@ class MS(object):
         """
         field_no = 0
         ant_no   = 0
-        with tables.table(self.ms + "/FIELD", ack = False) as field_table:
+        with tables.table(self.pathMS + "/FIELD", ack = False) as field_table:
             direction = field_table.getcol("PHASE_DIR")
-            ra        = direction[ ant_no, field_no, 0 ]
-            dec       = direction[ ant_no, field_no, 1 ]
-        
+            ra        = direction[ant_no, field_no, 0]
+            dec       = direction[ant_no, field_no, 1]
+
         if (ra < 0):
             ra += 2 * np.pi
-        
-        logging.debug("%s: Phase centre: %f deg - %f deg", self.ms, np.degrees(ra), np.degrees(dec))
-        #logger.debug("%s: Phase centre: %f deg - %f deg", self.ms, np.degrees(ra), np.degrees(dec))
+
+        logging.debug("%s: phase centre (deg): %f - %f", self.pathMS, np.degrees(ra), np.degrees(dec))
+        #logger.debug("%s: phase centre (deg): %f - %f", self.pathMS, np.degrees(ra), np.degrees(dec))
         return (np.degrees(ra), np.degrees(dec))
-    
-    
+
+
     '''
     import lib_util
-    
+
     def get_calname(self):
         """
         Check if MS is of a calibrator and return patch name
         """
         RA, Dec                 = self.get_phase_centre()
-        
+
         # The following list should be expanded to include all calibrators that are possibly used.
         calibratorRAs           = np.array([24.4220808, 85.6505746, 277.3824204, 212.835495, 123.4001379, 299.8681525, 202.784479167]) # in degrees
         calibratorDecs          = np.array([33.1597594, 49.8520094, 48.7461556,  52.202770,  48.2173778,  40.7339156,  30.509088])     # in degrees
         calibratorNames         = np.array(["3C48",     "3C147",    "3C380",     "3C295",    "3C196",     "CygA",      "3C286"])
-        
+
         distances               = lib_util.distanceOnSphere(RA, Dec, calibratorRAs, calibratorDecs)
-        
+
         distanceCutoff          = 1                                                                                                       # in degrees
-        
+
         calibratorIsNear        = (distances < distanceCutoff)
         numberOfCalibratorsNear = np.sum(calibratorIsNear)
-        
+
         if (numberOfCalibratorsNear == 0):
             logger.info("Error: unknown calibrator.")
             sys.exit()
@@ -162,5 +171,5 @@ class MS(object):
             return calibratorNameCurrent
         else:
             logger.info("Error: multiple calibrators were nearby.")
-            sys.exit()         
+            sys.exit()
     '''
--- pipeline_uGMRT_flag.py
+++ pipeline_uGMRT_flag.py
@@ -12,51 +12,63 @@ Notes:
 Paths to directories do not end with a '/'.
 '''
 
-import glob, logging
+import argparse, glob, logging
 
 import lib_ms, lib_util
 
 
-def main():
+def pipeline_uGMRT_flag(pathsMS, pathDirectoryLog, pathDirectoryParSet = "./parsets",
+                        flagBaselinesUser = "", flagFrequencyRangesUser = "[]", flagChannelsUser = "[]"):
+
     # Initialise parameter set settings.
-    pathDirectoryParSet = "/disks/strw3/oei/code/PiLF/parset_cal"
+    #pathDirectoryParSet = "/disks/strw3/oei/code/PiLF/parset_cal"
     nameParSetFlag      = "DPPP-uGMRT-flag.parset"
     pathParSetFlag      = pathDirectoryParSet + '/' + nameParSetFlag
-    
+
     # Initialise main folder setting.
     #pathDirectoryMain   = "/disks/strw3/oei/uGMRTCosmosCut-PiLF"
-    pathDirectoryMain   = "/disks/strw3/oei/uGMRTCosmosCut-PiLF/fieldsTarget/P149.7+03.4/MSs"
-    
+    #pathDirectoryMain   = "/disks/strw3/oei/uGMRTCosmosCut-PiLF/fieldsTarget/P149.7+03.4/MSs"
+
     # Initialise logging settings.
-    pathDirectoryLog    = "/disks/strw3/oei/uGMRTCosmosCut-PiLF/logs"
+    #pathDirectoryLog    = "/disks/strw3/oei/uGMRTCosmosCut-PiLF/logs"
     nameFileLog         = "pipeline_uGMRT_flag.log"
     pathFileLog         = pathDirectoryLog + '/' + nameFileLog
-    
-    # Initialise baselines to be flagged in entirety.
-    bl2flag             = ""
-    
-    
+
+
+
     lib_util.printLineBold("Starting log at '" + pathFileLog + "'...")
-    
+
     logging.basicConfig(filename = pathFileLog, level = logging.DEBUG)
     logging.info("Started 'pipeline_uGMRT_flag.py'!")
-    
-    
-    pathsMS = glob.glob(pathDirectoryMain + "/*MS")
-    
+
+
+    #pathsMS = glob.glob(pathDirectoryMain + "/*MS")
+
     for pathMS in pathsMS:
         MSObject = lib_ms.MS(pathMS)
         print (MSObject.find_nchan())
         print (MSObject.find_chanband())
-    
-    
+
+
     logging.info('Flagging...')
 
     scheduler = lib_util.Scheduler(dry = False, log_dir = pathDirectoryLog)
     MSs       = lib_ms.AllMSs(pathsMS, scheduler)
-    
-    MSs.run("DPPP " + pathParSetFlag + " msin=$ms flag1.baseline=" + bl2flag, log = nameFileLog, cmd_type = "DPPP")
+
+    MSs.run(command = "DPPP " + pathParSetFlag + " msin=$ms flagBaselines.baseline=" + bl2flag, log = nameFileLog, commandType = "DPPP")
 
 
 if (__name__ == "__main__"):
-    main()
+
+    # If the program is run from the command line, parse arguments.
+    parser    = argparse.ArgumentParser(description = "Pipeline step 2: Initial flagging of uGMRT data.")
+
+    parser.add_argument("pathsMS", help = "Paths to the MSs to act upon. Default: []")
+    parser.add_argument("-v", "--verbose", default = False, help = "Whether or not to provide extensive textual diagnostic output. Default: False")
+    arguments = parser.parse_args()
+
+    lib_util.printLineBold("Parameter set:")
+    print (arguments)
+
+    # Run the program with appropriate input.
+    pipeline_uGMRT_flag(arguments.pathsMS, pathDirectoryMS = arguments.directory, verbose = arguments.verbose)
