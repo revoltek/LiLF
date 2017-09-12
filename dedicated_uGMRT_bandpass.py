aff3f57e8fe799699ee4f9a6400f38368bbad3a8
Martijn Oei <Martijn@l-145-118-226-149.leidenuniv.nl>
2017-09-12 19:17:12 +0200

    losoto 2 import

--- dedicated_uGMRT_bandpass.py
+++ dedicated_uGMRT_bandpass.py
@@ -121,6 +121,7 @@ def dedicated_uGMRT_bandpass(pathH5Parm, pathDirectoryPlots, referenceAntennaID 
     objectSolSet = objectH5Parm.getSolset("sol000")
     namesSolTabs = objectSolSet.getSoltabNames()
     print (namesSolTabs)
+    print (namesSolTabs, "JAA!")
     import sys
     sys.exit()
 
