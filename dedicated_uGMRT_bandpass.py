1c767b99667911665b6367ab5967d2354492cefd
Martijn Oei <Martijn@MacBook-Pro-van-Martijn.local>
2017-09-15 18:10:23 +0200

    phase bandpass saving

--- dedicated_uGMRT_bandpass.py
+++ dedicated_uGMRT_bandpass.py
@@ -817,6 +817,7 @@ def dedicated_uGMRT_bandpass(pathDirectoryMS, referenceAntennaID = 0, verbose = 
     cubeBandpassAmplitudeWeights = numpy.logical_not(numpy.isnan(cubeBandpassAmplitudeValues))
 
     cubeBandpassPhaseValues      = numpy.array([bandpassesPhasePol1Iter2, bandpassesPhasePol2Iter2])
+    print (numpy.array(bandpassesAmplitudePol1Iter2).shape)
     print (type(cubeBandpassAmplitudeValues))
     print (type(cubeBandpassPhaseValues))
     print (cubeBandpassAmplitudeValues.shape)
