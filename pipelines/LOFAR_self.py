073864c4d23fa02464dad866e47e8a5077952552
Francesco Gasperin <rev@voo.it>
2019-08-14 23:44:48 +0200

    update

--- pipelines/LOFAR_self.py
+++ pipelines/LOFAR_self.py
@@ -191,7 +191,7 @@ for c in range(2):
     imagename = 'img/wideM-'+str(c)
     lib_util.run_wsclean(s, 'wsclean-c'+str(c)+'.log', MSs.getStrWsclean(), do_predict=True, name=imagename, save_source_list='', size=imgsizepix, scale='10arcsec', \
             weight='briggs -0.3', niter=1000000, no_update_model_required='', minuv_l=30, maxuv_l=5000, mgain=0.85, \
-            parallel_deconvolution=256, baseline_averaging=5, local_rms='', auto_threshold=1.5, auto_mask=3, \
+            parallel_deconvolution=256, baseline_averaging=5, local_rms='', auto_threshold=1.5, auto_mask=2.5, \
             join_channels='', fit_spectral_pol=3, channels_out=9, deconvolution_channels=3)
     os.system('cat logs/wsclean-c'+str(c)+'.log | grep "background noise"')
        
