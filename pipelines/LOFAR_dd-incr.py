0284095606385558c5ebd94d4ac4f5948d1c5be2
revoltek <rev@voo.it>
2019-08-23 10:17:53 +0000

    update

--- pipelines/LOFAR_dd-incr.py
+++ pipelines/LOFAR_dd-incr.py
@@ -92,7 +92,7 @@ def clean(p, MSs, size, res='normal', apply_beam=False):
         lib_util.run_wsclean(s, 'wscleanB-'+str(p)+'.log', MSs.getStrWsclean(), name=imagename, save_source_list='', size=imsize, scale=str(pixscale)+'arcsec', \
             weight=weight, niter=100000, no_update_model_required='', minuv_l=30, maxuv_l=maxuv_l, mgain=0.85, \
             use_idg='', grid_with_beam='', use_differential_lofar_beam='', beam_aterm_update=400, \
-            multiscale='', multiscale_bias=0.75, \
+            multiscale='', multiscale_scale_bias=0.75, \
             parallel_deconvolution=256, local_rms='', auto_threshold=1.5, auto_mask=3, fits_mask=im.maskname, \
             join_channels='', fit_spectral_pol=3, channels_out=9, deconvolution_channels=3)
 
@@ -100,7 +100,7 @@ def clean(p, MSs, size, res='normal', apply_beam=False):
 
         lib_util.run_wsclean(s, 'wscleanB-'+str(p)+'.log', MSs.getStrWsclean(), name=imagename, size=imsize, save_source_list='', scale=str(pixscale)+'arcsec', \
             weight=weight, niter=50000, no_update_model_required='', minuv_l=30, maxuv_l=maxuv_l, mgain=0.85, \
-            multiscale='', multiscale_bias=0.75, \
+            multiscale='', multiscale_scale_bias=0.75, \
             baseline_averaging=5, parallel_deconvolution=256, local_rms='', auto_threshold=1.5, auto_mask=3, fits_mask=im.maskname, \
             join_channels='', fit_spectral_pol=3, channels_out=9, deconvolution_channels=3)
 
