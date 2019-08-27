10e1b131811d8a283aa06ec824fee8e9216ae522
Francesco Gasperin fdg@hs.uni-hamburg.de <baq1889@node31.lofar.gpu.cluster>
2019-08-27 10:11:59 +0200

    update

--- pipelines/LOFAR_3c.py
+++ pipelines/LOFAR_3c.py
@@ -229,9 +229,9 @@ for c in range(100):
     os.system('cp -r img/imgM-%02i-MFS-model.fits img/imgMbkp-%02i-MFS-model.fits' % (c,c))
     os.system('cp -r img/imgM-%02i-MFS-residual.fits img/imgMbkp-%02i-MFS-residual.fits' % (c,c))
     lib_util.run_wsclean(s, 'wsclean-c'+str(c)+'.log', MSs.getStrWsclean(), do_predict=True, cont=True, name=imagename, size=2000, scale='1arcsec', \
-            weight='briggs -1', niter=1000000, no_update_model_required='', minuv_l=30, mgain=0.8, nmiter=0, \
-            auto_threshold=0.5, auto_mask=2, local_rms='', \
-            multiscale='', multiscale_scale_bias=0.75, \
+            weight='briggs -1', niter=1000000, no_update_model_required='', minuv_l=30, mgain=0.7, nmiter=0, \
+            auto_threshold=0.5, auto_mask=2.5, local_rms='', \
+            multiscale='', multiscale_scale_bias=0.7, \
             join_channels='', fit_spectral_pol=2, channels_out=2 )
     os.system('cat logs/wsclean-c'+str(c)+'.log | grep "background noise"')
 
