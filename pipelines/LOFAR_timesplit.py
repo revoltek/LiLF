c7fb589eeb5d3d08c357aa851eeafadada41d3e6
Francesco Gasperin fdg@hs.uni-hamburg.de <baq1889@node31.lofar.gpu.cluster>
2020-03-26 12:27:43 +0100

    resume

--- pipelines/LOFAR_timesplit.py
+++ pipelines/LOFAR_timesplit.py
@@ -30,15 +30,24 @@ if 'LBAsurvey' in os.getcwd():
 
 #################################################
 # Clean
-logger.info('Cleaning...')
-lib_util.check_rm('mss*')
-MSs = lib_ms.AllMSs( glob.glob(data_dir+'/*MS'), s )
+if w.todo('clean'):
+    logger.info('Cleaning...')
+    lib_util.check_rm('mss*')
 
-logger.info('Copy data...')
-for MS in MSs.getListObj():
-    if min(MS.getFreqs()) > 30.e6:
-        # overwrite=True to prevent updating the weights twice
-        MS.move(MS.nameMS+'.MS', keepOrig=True, overwrite=True)
+    w.done('clean')
+### DONE
+
+if w.todo('copy'):
+    MSs = lib_ms.AllMSs( glob.glob(data_dir+'/*MS'), s )
+
+    logger.info('Copy data...')
+    for MS in MSs.getListObj():
+        if min(MS.getFreqs()) > 30.e6:
+            # overwrite=True to prevent updating the weights twice
+            MS.move(MS.nameMS+'.MS', keepOrig=True, overwrite=True)
+
+    w.done('copy')
+### DONE
 
 MSs = lib_ms.AllMSs( glob.glob('*MS'), s )
 
@@ -61,21 +70,25 @@ assert os.path.exists(h5_iono)
 
 ####################################################
 # Correct fist for BP(diag)+TEC+Clock and then for beam
+if w.todo('apply'):
+    
+    # Apply cal sol - SB.MS:DATA -> SB.MS:CORRECTED_DATA (polalign corrected)
+    logger.info('Apply solutions (pa)...')
+    MSs.run('DPPP '+parset_dir+'/DPPP-cor.parset msin=$pathMS \
+            cor.parmdb='+h5_pa+' cor.correction=polalign', log='$nameMS_cor1.log', commandType='DPPP')
+    
+    # Apply cal sol - SB.MS:CORRECTED_DATA -> SB.MS:CORRECTED_DATA (polalign corrected, calibrator corrected+reweight, beam corrected+reweight)
+    logger.info('Apply solutions (amp/ph)...')
+    MSs.run('DPPP '+parset_dir+'/DPPP-cor.parset msin=$pathMS msin.datacolumn=CORRECTED_DATA cor.steps=[amp,ph] \
+            cor.amp.parmdb='+h5_amp+' cor.amp.correction=amplitudeSmooth cor.amp.updateweights=True\
+            cor.ph.parmdb='+h5_iono+' cor.ph.correction=phaseOrig000', log='$nameMS_cor2.log', commandType='DPPP')
+    
+    # Beam correction CORRECTED_DATA -> CORRECTED_DATA (polalign corrected, beam corrected+reweight)
+    logger.info('Beam correction...')
+    MSs.run('DPPP '+parset_dir+'/DPPP-beam.parset msin=$pathMS corrbeam.updateweights=True', log='$nameMS_beam.log', commandType='DPPP')
 
-# Apply cal sol - SB.MS:DATA -> SB.MS:CORRECTED_DATA (polalign corrected)
-logger.info('Apply solutions (pa)...')
-MSs.run('DPPP '+parset_dir+'/DPPP-cor.parset msin=$pathMS \
-        cor.parmdb='+h5_pa+' cor.correction=polalign', log='$nameMS_cor1.log', commandType='DPPP')
-
-# Apply cal sol - SB.MS:CORRECTED_DATA -> SB.MS:CORRECTED_DATA (polalign corrected, calibrator corrected+reweight, beam corrected+reweight)
-logger.info('Apply solutions (amp/ph)...')
-MSs.run('DPPP '+parset_dir+'/DPPP-cor.parset msin=$pathMS msin.datacolumn=CORRECTED_DATA cor.steps=[amp,ph] \
-        cor.amp.parmdb='+h5_amp+' cor.amp.correction=amplitudeSmooth cor.amp.updateweights=True\
-        cor.ph.parmdb='+h5_iono+' cor.ph.correction=phaseOrig000', log='$nameMS_cor2.log', commandType='DPPP')
-
-# Beam correction CORRECTED_DATA -> CORRECTED_DATA (polalign corrected, beam corrected+reweight)
-logger.info('Beam correction...')
-MSs.run('DPPP '+parset_dir+'/DPPP-beam.parset msin=$pathMS corrbeam.updateweights=True', log='$nameMS_beam.log', commandType='DPPP')
+    w.done('apply')
+### DONE
 
 ###################################################################################################
 # Create groups
@@ -94,78 +107,97 @@ for timechunk in timechunks:
         else:
             groupname = 'mss_t%s-%02i' % (timechunk, i)
         groupnames.append(groupname)
-        lib_util.check_rm(groupname)
-        os.makedirs(groupname)
 
-        # add missing SB with a fake name not to leave frequency holes
-        num_init = int(re.findall(r'\d+', msg[0])[-1])
-        num_fin = int(re.findall(r'\d+', msg[-1])[-1])
-        ms_name_init = msg[0]
-        msg = []
-        for j in range(num_init, num_fin+1):
-            msg.append(ms_name_init.replace('SB%03i' % num_init, 'SB%03i' % j))
-
-        # prepare concatenated time chunks (TC) - SB.MS:CORRECTED_DATA -> group#.MS:DATA (cal corr data, beam corrected, circular)
-        s.add('DPPP '+parset_dir+'/DPPP-concat.parset msin="['+','.join(msg)+']"  msout='+groupname+'/'+groupname+'.MS', \
-                    log=groupname+'_DPPP_concat.log', commandType='DPPP')
+        # skip if already done
+        if not os.path.exists(groupname):
+            os.makedirs(groupname)
+    
+            # add missing SB with a fake name not to leave frequency holes
+            num_init = int(re.findall(r'\d+', msg[0])[-1])
+            num_fin = int(re.findall(r'\d+', msg[-1])[-1])
+            ms_name_init = msg[0]
+            msg = []
+            for j in range(num_init, num_fin+1):
+                msg.append(ms_name_init.replace('SB%03i' % num_init, 'SB%03i' % j))
+    
+            # prepare concatenated time chunks (TC) - SB.MS:CORRECTED_DATA -> group#.MS:DATA (cal corr data, beam corrected, circular)
+            s.add('DPPP '+parset_dir+'/DPPP-concat.parset msin="['+','.join(msg)+']"  msout='+groupname+'/'+groupname+'.MS', \
+                        log=groupname+'_DPPP_concat.log', commandType='DPPP')
     s.run(check=True)
 
 MSs = lib_ms.AllMSs( glob.glob('mss_t*/*MS'), s )
 
+#############################################################
 # Flagging on concatenated dataset - also flag low-elevation
-logger.info('Flagging...')
-MSs.run('DPPP '+parset_dir+'/DPPP-flag.parset msin=$pathMS ant.baseline=\"' + bl2flag + '\"', \
-                log='$nameMS_DPPP_flag.log', commandType='DPPP')
+if w.todo('flag'):
 
-logger.info('Remove bad timestamps...')
-MSs.run( 'flagonmindata.py -f 0.5 $pathMS', log='$nameMS_flagonmindata.log', commandType='python')
+    logger.info('Flagging...')
+    MSs.run('DPPP '+parset_dir+'/DPPP-flag.parset msin=$pathMS ant.baseline=\"' + bl2flag + '\"', \
+                    log='$nameMS_DPPP_flag.log', commandType='DPPP')
+    
+    logger.info('Remove bad timestamps...')
+    MSs.run( 'flagonmindata.py -f 0.5 $pathMS', log='$nameMS_flagonmindata.log', commandType='python')
+    
+    logger.info('Plot weights...')
+    MSs.run('reweight.py $pathMS -v -p -a CS001LBA', log='$nameMS_weights.log', commandType='python')
+    os.system('mkdir plots-weights; mv *png plots-weights')
 
-logger.info('Plot weights...')
-MSs.run('reweight.py $pathMS -v -p -a CS001LBA', log='$nameMS_weights.log', commandType='python')
-os.system('mkdir plots-weights; mv *png plots-weights')
+    w.done('flag')
+### DONE
 
 #sys.exit() # for DDFacet
 
+#####################################
 # Create time-chunks
-logger.info('Splitting in time...')
-tc = initc
-for groupname in groupnames:
-    ms = groupname+'/'+groupname+'.MS'
-    if not os.path.exists(ms): continue
-    t = pt.table(ms, ack=False)
-    starttime = t[0]['TIME']
-    endtime   = t[t.nrows()-1]['TIME']
-    hours = (endtime-starttime)/3600.
-    logger.debug(ms+' has length of '+str(hours)+' h.')
+if w.todo('timesplit'):
 
-    for timerange in np.array_split(sorted(set(t.getcol('TIME'))), round(hours)):
-        logger.info('%02i - Splitting timerange %f %f' % (tc, timerange[0], timerange[-1]))
-        t1 = t.query('TIME >= ' + str(timerange[0]) + ' && TIME <= ' + str(timerange[-1]), sortlist='TIME,ANTENNA1,ANTENNA2')
-        splitms = groupname+'/TC%02i.MS' % tc
-        lib_util.check_rm(splitms)
-        t1.copy(splitms, True)
-        t1.close()
-        tc += 1
-    t.close()
+    logger.info('Splitting in time...')
+    tc = initc
+    for groupname in groupnames:
+        ms = groupname+'/'+groupname+'.MS'
+        if not os.path.exists(ms): continue
+        t = pt.table(ms, ack=False)
+        starttime = t[0]['TIME']
+        endtime   = t[t.nrows()-1]['TIME']
+        hours = (endtime-starttime)/3600.
+        logger.debug(ms+' has length of '+str(hours)+' h.')
+    
+        for timerange in np.array_split(sorted(set(t.getcol('TIME'))), round(hours)):
+            logger.info('%02i - Splitting timerange %f %f' % (tc, timerange[0], timerange[-1]))
+            t1 = t.query('TIME >= ' + str(timerange[0]) + ' && TIME <= ' + str(timerange[-1]), sortlist='TIME,ANTENNA1,ANTENNA2')
+            splitms = groupname+'/TC%02i.MS' % tc
+            lib_util.check_rm(splitms)
+            t1.copy(splitms, True)
+            t1.close()
+            tc += 1
+        t.close()
+    
+        lib_util.check_rm(ms) # remove not-timesplitted file
 
-    lib_util.check_rm(ms) # remove not-timesplitted file
+    w.done('timesplit')
+### DONE
 
-
+############################################
 # put everything together
-if ngroups == 1:
-    lib_util.check_rm('mss')
-    os.makedirs('mss')
-    os.system('mv mss_t*/*MS mss')
-    lib_util.check_rm('mss_t*')
-else:
-    for group in range(ngroups):
-        groupname = 'mss-%02i' % group
-        lib_util.check_rm(groupname)
-        os.makedirs(groupname)
-        os.system('mv mss_t*-%02i/*MS %s' % (group, groupname))
-    lib_util.check_rm('mss_t*')
+if w.todo('concat'):
 
-logger.info('Cleaning up...')
-os.system('rm -r *MS')
+    if ngroups == 1:
+        lib_util.check_rm('mss')
+        os.makedirs('mss')
+        os.system('mv mss_t*/*MS mss')
+        lib_util.check_rm('mss_t*')
+    else:
+        for group in range(ngroups):
+            groupname = 'mss-%02i' % group
+            lib_util.check_rm(groupname)
+            os.makedirs(groupname)
+            os.system('mv mss_t*-%02i/*MS %s' % (group, groupname))
+        lib_util.check_rm('mss_t*')
+    
+    logger.info('Cleaning up...')
+    os.system('rm -r *MS')
+
+    w.done('concat')
+### DONE
 
 logger.info("Done.")
