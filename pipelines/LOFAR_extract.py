a1042d0a8a16f6de2993299c2249c9d445189422
Henrik Edler <henrik.edler@hs.uni-hamburg.de>
2020-12-11 14:20:44 +0100

    add beam corr

--- pipelines/LOFAR_extract.py
+++ pipelines/LOFAR_extract.py
@@ -40,7 +40,7 @@ with w.if_todo('cleaning'):
         logger.info('Copy MS...')
         os.system('cp -r mss-avg mss-extract')
 
-# for now, region must be a single ds9 circle
+# region must be a single ds9 circle
 target_reg = pyregion.open(target_reg_file)
 if len(target_reg) > 1 or target_reg[0].name != 'circle':
     raise ImportError('Only single circle regions supported.')
@@ -50,6 +50,7 @@ logger.info("Extract target region: {:.2f}deg, {:.2f}deg; radius: {:.3f}deg".for
 MSs = lib_ms.AllMSs( glob.glob('mss-extract/*MS'), s )
 ch_out = MSs.getChout(4e6)  # chout from dd-serial
 fwhm = MSs.getListObj()[0].getFWHM(freq='mid')
+phase_center = MSs.getListObj()[0].getPhaseCentre()
 # read image, h5parm, make mask
 wideDD_image = lib_img.Image('extract/init/wideDD-c01.app.restored.fits')
 dde_h5parm = 'extract/init/interp.h5'
@@ -96,7 +97,6 @@ def clean(p, MSs, res='normal', size=[1, 1], empty=False, imagereg=None):
         sys.exit()
 
     if empty:
-
         logger.info('Cleaning empty (' + str(p) + ')...')
         imagename = 'img/empty-' + str(p)
         lib_util.run_wsclean(s, 'wscleanE-' + str(p) + '.log', MSs.getStrWsclean(), name=imagename,
@@ -118,7 +118,7 @@ def clean(p, MSs, res='normal', size=[1, 1], empty=False, imagereg=None):
         # make mask
         im = lib_img.Image(imagename + '-MFS-image.fits', userReg=userReg)
         try:
-            im.makeMask(threshpix=6, rmsbox=(70, 5)) # threshpix lowered to 6 since we have pretty good starting cal alrdy
+            im.makeMask(threshpix=10, rmsbox=(70, 5))
         except:
             logger.warning('Fail to create mask for %s.' % imagename + '-MFS-image.fits')
             return
@@ -134,13 +134,10 @@ def clean(p, MSs, res='normal', size=[1, 1], empty=False, imagereg=None):
                              size=imsize, scale=str(pixscale) + 'arcsec',
                              weight=weight, niter=100000, no_update_model_required='', minuv_l=30, maxuv_l=maxuv_l,
                              reuse_psf=imagename, reuse_dirty=imagename, mgain=0.85, multiscale='', baseline_averaging='',
-                             auto_threshold=0.5, auto_mask=1.5,
+                             parallel_deconvolution=512, auto_threshold=0.5, auto_mask=1.5,
                              fits_mask=im.maskname, join_channels='', fit_spectral_pol=3, channels_out=ch_out)  # , deconvolution_channels=3)
-
         os.system('cat logs/wscleanB-' + str(p) + '.log | grep "background noise"')
 
-
-### DONE
 
 with w.if_todo('predict_rest'):
     # DDF predict+corrupt in MODEL_DATA of everything BUT the calibrator
@@ -185,7 +182,7 @@ with w.if_todo('predict_rest'):
     }
     logger.info('Predict corrupted rest-of-the-sky...')
     lib_util.run_DDF(s, 'ddfacet-pre.log', **ddf_parms, Cache_Reset=1)
-### DONE
+    ### DONE
 
 with w.if_todo('subtract_rest'):
     # Remove corrupted data from CORRECTED_DATA
@@ -194,21 +191,31 @@ with w.if_todo('subtract_rest'):
     logger.info('Set SUBTRACTED_DATA = CORRECTED_DATA - MODEL_DATA...')
     MSs.run('taql "update $pathMS set SUBTRACTED_DATA = CORRECTED_DATA - MODEL_DATA"',
             log='$nameMS_subtract.log', commandType='general')
+    ### DONE
+
 ## TTESTTESTTEST: empty image
 if not os.path.exists('img/empty-but-target-image.fits'):
     clean('but-target', MSs, size=(fwhm,fwhm), res='normal', empty=True)
+    ### DONE
 
 
 # Phase shift in the target location
 with w.if_todo('phaseshift'):
     logger.info('Phase shift and avg...')
-    lib_util.check_rm('mss-extract/*MS-small')
     MSs.run('DPPP '+parset_dir+'/DPPP-shiftavg.parset msin=$pathMS msout=mss-extract/$nameMS.MS-extract msin.datacolumn=SUBTRACTED_DATA \
             shift.phasecenter=['+str(target_reg.coord_list[0])+'deg,'+str(target_reg.coord_list[1])+'deg\] avg.freqstep=8 avg.timestep=2', \
             log='$nameMS_shiftavg.log', commandType='DPPP')
-### DONE
+    ### DONE
 
 MSs = lib_ms.AllMSs( glob.glob('mss-extract/*MS-extract'), s )
+
+with w.if_todo('%s-beamcorr'):
+    logger.info('Correcting beam...')
+    # Convince DPPP that DATA is corrected for the beam in the phase centre
+    MSs.run('DPPP ' + parset_dir + '/DPPP-beam.parset msin=$pathMS setbeam.direction=\[' + str(phase_center[0]) + 'deg,'
+            + str(phase_center[1]) + 'deg\] corrbeam.direction=\[' + str(target_reg.coord_list[0]) + 'deg,' + str(
+        str(target_reg.coord_list[1])) + 'deg\]', log='$nameMS_beam-.log', commandType='DPPP')
+    ### DONE
 
 # apply init - closest DDE sol
 # TODO: this assumes phase000 and optionally, amplitude000
@@ -222,8 +229,8 @@ with w.if_todo('apply_init'):
     closest = solset_dde.getSoltab('phase000').dir[np.argmin(dir_dist)]
     logger.info('Init apply: correct closest DDE solutions ({})'.format(closest))
     logger.info('Correct init ph...')
-    MSs.run('DPPP ' + parset_dir + '/DPPP-correct.parset msin=$pathMS msin.datacolumn=DATA msout.datacolumn=CORRECTED_DATA \
-                 cor.parmdb=' + dde_h5parm + ' cor.correction=phase000 cor.direction='+closest,
+    MSs.run('DPPP ' + parset_dir + '/DPPP-correct.parset msin=$pathMS msin.datacolumn=DATA '
+                                   'msout.datacolumn=CORRECTED_DATA \ cor.parmdb=' + dde_h5parm + ' cor.correction=phase000 cor.direction='+closest,
             log='$nameMS_init-correct.log', commandType='DPPP')
     if 'amplitude000' in solset_dde.getSoltabNames():
         logger.info('Correct init amp...')
@@ -231,45 +238,38 @@ with w.if_todo('apply_init'):
                      cor.parmdb=' + dde_h5parm + ' cor.correction=amplitude000 cor.direction=' + closest,
                 log='$nameMS_init-correct.log', commandType='DPPP')
     h5init.close()
-### DONE
+    ### DONE
 
 # initial imaging to get the model in the MODEL_DATA (could also be done using the Dico DDFacet model
 with w.if_todo('image_init'):
     logger.info('Initial imaging...')
     clean('init', MSs, size=(2*target_reg.coord_list[2], 2*target_reg.coord_list[2]))
-### DONE
+    ### DONE
 
 # Smoothing - ms:DATA -> ms:SMOOTHED_DATA
 with w.if_todo('smooth'):
     logger.info('BL-based smoothing...')
     # try smoothing less (does the averaging affect smoothing?)
-    MSs.run('BLsmooth.py -c 1 -n 8 -r -f 0.01 -i DATA -o SMOOTHED_DATA $pathMS', log='$nameMS_smooth.log', commandType='python') #, maxThreads=1)
-### DONE
+    MSs.run('BLsmooth.py -c 1 -n 8 -r -i DATA -o SMOOTHED_DATA $pathMS', log='$nameMS_smooth.log', commandType='python') #, maxThreads=1)
+    ### DONE
 
 # get initial noise and set iterators for timeint solutions
 image = lib_img.Image('img/extractM-init-MFS-image.fits', userReg=userReg)
-rms_noise_pre = image.getNoise();
-rms_noise_init = rms_noise_pre
-mm_ratio_pre = image.getMaxMinRatio();
-mm_ratio_init = mm_ratio_pre
+rms_noise_pre, mm_ratio_pre = image.getNoise(), image.getMaxMinRatio()
+rms_noise_init, mm_ratio_init = rms_noise_pre, mm_ratio_pre
 doamp = False
+
 # usually there are 3600/30=120 or 3600/15=240 timesteps, try to use multiple numbers
 # integration time twice as long as DD-serial
 iter_ph_solint = lib_util.Sol_iterator([8, 8, 4, 2, 1])
-iter_amp_solint = lib_util.Sol_iterator([60, 30, 15, 10])
-iter_amp2_solint = lib_util.Sol_iterator([120, 60, 30])
+iter_amp_solint = lib_util.Sol_iterator([60, 60, 30, 15])
+iter_amp2_solint = lib_util.Sol_iterator([120, 60, 60, 30])
 logger.info('RMS noise (init): %f' % (rms_noise_pre))
 logger.info('MM ratio (init): %f' % (mm_ratio_pre))
 rms_noise_pre = np.inf
 
-# TODO: any beam correction/corruption to consider for off-center target?
-
 for c in range(maxniter):
     logger.info('Starting cycle: %i' % c)
-
-    with w.if_todo('smooth-c%02i' % c):
-        logger.info('BL-based smoothing on MODEL_DATA...')
-        MSs.run('BLsmooth.py -c 1 -n 8 -f 0.002 -r -i MODEL_DATA -o MODEL_DATA $pathMS', log='$nameMS_smoothM-c%02i.log' % c, commandType='python') #, maxThreads=1
 
     h5ph = 'extract/cal-ph-c%02i.h5' % c
     solint_ph = next(iter_ph_solint)
@@ -282,13 +282,23 @@ for c in range(maxniter):
     logger.info('Phase calibration...')
     with w.if_todo('cal-ph-c%02i' % c):
         MSs.run('DPPP ' + parset_dir + '/DPPP-solG.parset msin=$pathMS msin.datacolumn=SMOOTHED_DATA sol.h5parm=$pathMS/cal-ph.h5 \
-            sol.mode=scalarcomplexgain sol.solint=' + str(solint_ph) + ' sol.nchan=1 sol.smoothnessconstraint=5e6 \
+            sol.mode=scalarphase sol.solint=' + str(solint_ph) + ' sol.nchan=1 sol.smoothnessconstraint=5e6 \
             sol.antennaconstraint=[[CS001LBA,CS002LBA,CS003LBA,CS004LBA,CS005LBA,CS006LBA,CS007LBA,CS011LBA,CS013LBA,CS017LBA,CS021LBA,CS024LBA,CS026LBA,CS028LBA,CS030LBA,CS031LBA,CS032LBA,CS101LBA,CS103LBA,CS201LBA,CS301LBA,CS302LBA,CS401LBA,CS501LBA]]', \
-                    log='$nameMS_solGph-c%02i.log' % c, commandType='DPPP')
+                log='$nameMS_solGph-c%02i.log' % c, commandType='DPPP')
         lib_util.run_losoto(s, 'ph', [ms + '/cal-ph.h5' for ms in MSs.getListStr()],
                             [parset_dir + '/losoto-plot1.parset'],
                             plots_dir='extract/plots-%s' % c)
         os.system('mv cal-ph.h5 %s' % h5ph)
+
+    # with w.if_todo('cal-ph-c%02i' % c):
+    #     MSs.run('DPPP ' + parset_dir + '/DPPP-solG.parset msin=$pathMS msin.datacolumn=SMOOTHED_DATA sol.h5parm=$pathMS/cal-ph.h5 \
+    #         sol.mode=scalarcomplexgain sol.solint=' + str(solint_ph) + ' sol.nchan=1 sol.smoothnessconstraint=5e6 \
+    #         sol.antennaconstraint=[[CS001LBA,CS002LBA,CS003LBA,CS004LBA,CS005LBA,CS006LBA,CS007LBA,CS011LBA,CS013LBA,CS017LBA,CS021LBA,CS024LBA,CS026LBA,CS028LBA,CS030LBA,CS031LBA,CS032LBA,CS101LBA,CS103LBA,CS201LBA,CS301LBA,CS302LBA,CS401LBA,CS501LBA]]', \
+    #                 log='$nameMS_solGph-c%02i.log' % c, commandType='DPPP')
+    #     lib_util.run_losoto(s, 'ph', [ms + '/cal-ph.h5' for ms in MSs.getListStr()],
+    #                         [parset_dir + '/losoto-plot1.parset'],
+    #                         plots_dir='extract/plots-%s' % c)
+    #     os.system('mv cal-ph.h5 %s' % h5ph)
 
     with w.if_todo('cor-ph-c%02i' % c):
         # correct ph - ms:DATA -> ms:CORRECTED_DATA
