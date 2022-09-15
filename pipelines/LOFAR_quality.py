9c2911e87519d5dd0caae07fec4a9a62c4e7a840
Henrik Edler <henrik.edler@hs.uni-hamburg.de>
2022-09-15 14:46:25 +0200

    NVSS ratio in quality

--- pipelines/LOFAR_quality.py
+++ pipelines/LOFAR_quality.py
@@ -4,14 +4,16 @@
 import sys, os, glob, re, pickle
 from astropy.io.fits import getdata
 import numpy as np
+from astropy.table import Table
 from astropy.stats import median_absolute_deviation
+import bdsf
 
-# TODO: cross-match
 ########################################################
-from LiLF import lib_ms, lib_img, lib_util, lib_log
+from LiLF import lib_ms, lib_img, lib_util, lib_log, lib_cat
 logger_obj = lib_log.Logger('pipeline-quality.logger')
 logger = lib_log.logger
 s = lib_util.Scheduler(log_dir = logger_obj.log_dir, dry = False)
+w = lib_util.Walker('pipeline-quality.walker')
 
 # parse parset
 parset = lib_util.getParset()
@@ -40,12 +42,22 @@ def get_noise(fitsfile):
 
 
 #############################################################
+with w.if_todo('cleaning'):
+    logger.info('Cleaning...')
+    lib_util.check_rm('quality')
+    os.makedirs('quality')
+
+### DONE
+
 MSs = lib_ms.AllMSs( glob.glob('mss/TC*[0-9].MS'), s, check_flags=False)
-qdict = {'self_c0_rms': None, 'self_c1_rms': None, 'self_nvss_ratio':None, 'ddcal_c0_rms': None,
-                'ddcal_c1_rms': None, 'ddcal_nvss_ratio': None}
+ra, dec = MSs.getListObj()[0].getPhaseCentre()
+fwhm = MSs.getListObj()[0].getFWHM()
+qdict = {'self_c0_rms': None, 'self_c1_rms': None, 'ddcal_c0_rms': None,
+                'ddcal_c1_rms': None, 'nvss_ratio': None}
 # MS flags, count all flags and print %
 
 # self images [noise per cycle]
+
 if os.path.exists('self'):
     img_self_c0 = self_dir+'/images/wideM-0-MFS-residual.fits'
     qdict['self_c0_rms'] = get_noise(img_self_c0)
@@ -63,11 +75,33 @@ if os.path.exists('ddcal'):
     logger.info('DDcal residual rms noise (cycle 0): %.1f mJy/b' % (qdict['ddcal_c0_rms']*1e3))
     img_ddcal_c1 = sorted(glob.glob(ddcal_dir+'/c01/images/wideDD-c01.residual*.fits'))[-1]
     qdict['ddcal_c1_rms'] = get_noise(img_ddcal_c1)
-    logger.info('DDcal residual rms noise (cycle 1): %.1f mJy/b' % (qdict['ddcal_c1_rms']*1e3))
+    logger.info('DD-cal residual rms noise (cycle 1): %.1f mJy/b' % (qdict['ddcal_c1_rms']*1e3))
+
+    with w.if_todo('process_ddimage'):
+        os.chdir(f'{ddcal_dir}/c01/images/') # bdsf raises error if image not in wdir?
+        img = bdsf.process_image(f'wideDD-c01.int.restored.fits', detection_image=f'wideDD-c01.app.restored.fits',
+                                 thresh_isl=4.0, thresh_pix=5.0, rms_box=(150, 15),
+                                 rms_map=True, mean_map='zero', ini_method='intensity', adaptive_rms_box=True,
+                                 adaptive_thresh=50, rms_box_bright=(60, 15),group_by_isl=False, group_tol=10.0,
+                                 output_opts=True, output_all=True, atrous_do=False, atrous_jmax=4, flagging_opts=True,
+                                 flag_maxsize_fwhm=0.5, advanced_opts=True, blank_limit=None)
+        os.chdir('../../../')
+        img.write_catalog(outfile='quality/wideDD-c01.int.cat.fits', catalog_type='srl', format='fits', correct_proj='True',
+                          clobber=True)
+
+    with w.if_todo('find_sources'):
+        nvss = lib_cat.RadioCat(f'{parset_dir}/NVSS_small.fits', 'NVSS', log=logger, col_pflux=None, col_maj=None)
+        nvss.filter(sigma=5, circle=[ra, dec, fwhm/2], isolation=90)
+        lofar = lib_cat.RadioCat('quality/wideDD-c01.int.cat.fits', 'LOFAR', log=logger)
+        lofar.filter(sigma=5, circle=[ra, dec, fwhm/2], isolation=30, minflux=0.06, size=25)
+        lofar.match(nvss, 10)
+        lofar.write('quality/wideDD-c01.int.cat_match_nvss.fits', overwrite=True)
+        median_nvss_ratio = lofar.flux_ratios('NVSS')
+        qdict['nvss_ratio'] = median_nvss_ratio
 else:
     logger.warning('Skip "ddcal" tests, missing dir.')
 
-with open('quality.pickle', 'wb') as f:
+with open('quality/quality.pickle', 'wb') as f:
     pickle.dump(qdict, f)
 
 logger.info("Done.")
