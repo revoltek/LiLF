3ce19526829d16bfeabf3d964c8fd14c4aafd2ac
Henrik Edler <henrik.edler@hs.uni-hamburg.de>
2024-03-13 17:02:56 +0100

    fix demix-avg error

--- pipelines/LOFAR_preprocess.py
+++ pipelines/LOFAR_preprocess.py
@@ -25,7 +25,7 @@ fix_table = parset.getboolean('LOFAR_preprocess','fix_table')
 renameavg = parset.getboolean('LOFAR_preprocess','renameavg')
 keep_IS = parset.getboolean('LOFAR_preprocess','keep_IS')
 backup_full_res = parset.getboolean('LOFAR_preprocess','backup_full_res')
-demix_sources = parset.get('LOFAR_preprocess','demix_sources') # Demix  sources in these patches (e.g. [VirA,TauA], default: No demix
+demix_sources = parset.get('LOFAR_preprocess','demix_sources') # Demix  sources in these patches (e.g. CasA or [VirA,TauA]), default: No demix
 demix_skymodel = parset.get('LOFAR_preprocess','demix_skymodel') # Use non-default demix skymodel
 demix_field_skymodel = parset.get('LOFAR_preprocess','demix_field_skymodel') # provide a custom target skymodel instead of online gsm model
 
@@ -135,6 +135,7 @@ if renameavg:
         with open('renamed.txt','a') as flog:
             MSs = lib_ms.AllMSs([MS for MS in glob.glob('*MS') if not os.path.exists(getName(MS))], s, check_flags=False)
             minfreq = np.min(MSs.getFreqs())
+            fwhm = MSs.getListObj()[0].getFWHM(freq='min') # for radius of model
             logger.info('Min freq: %.2f MHz' % (minfreq/1e6))
             for MS in MSs.getListObj():
 
@@ -214,7 +215,6 @@ if renameavg:
                         # if not provided, use GSM
                         if demix_field_skymodel == '':
                             logger.info('Include target from GSM...')
-                            fwhm = MSs.getListObj()[0].getFWHM(freq='min')
                             # get model the size of the image (radius=fwhm/2)
                             os.system('wget -O demix_tgts.skymodel "https://lcs165.lofar.eu/cgi-bin/gsmv1.cgi?coord=%f,%f&radius=%f&unit=deg"' % (
                                     ra, dec, fwhm/2))  # ASTRON
