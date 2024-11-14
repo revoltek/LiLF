70224b018a72a6dc4b6665a6418f1998a8588b9a
JortBox <boxelaar@strw.leidenuniv.nl>
2024-11-14 12:06:37 +0100

    reduce 3C overhead

--- LiLF/lib_util.py
+++ LiLF/lib_util.py
@@ -93,7 +93,7 @@ def getParset(parsetFile=''):
     add_default('LOFAR_ddparallel', 'subfield_min_flux', '20') # min flux within calibration subfield
     add_default('LOFAR_ddparallel', 'ph_sol_mode', 'phase') # phase or tecandphase
     add_default('LOFAR_ddparallel', 'intrinsic', 'True')
-    add_default('LOFAR_ddparallel', 'remove3c', 'False')
+    add_default('LOFAR_ddparallel', 'remove3c', 'True')
     # dd
     add_default('LOFAR_ddserial', 'maxIter', '2')
     add_default('LOFAR_ddserial', 'minCalFlux60', '0.7')
