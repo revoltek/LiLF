e4079a122b97b37721a2df5dd65581f2eeb27f49
AlexKurek <aleksander.kurek@uj.edu.pl>
2025-02-12 09:02:03 +0100

    Update label in docker_build.sh

--- container/docker_build.sh
+++ container/docker_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # docker image prune -a
 # docker system prune -a
-docker build . -t revoltek/pill:20240905 > docker_build.log 2>&1
-docker push revoltek/pill:20240905
-singularity build pill.simg docker://revoltek/pill:20240905
+docker build . -t revoltek/pill:20250113 > docker_build.log 2>&1
+docker push revoltek/pill:20250113
+singularity build pill.simg docker://revoltek/pill:20250113
 singularity run --pid --writable-tmpfs --containall --cleanenv -B/home/fdg:/home/fdg,/export/scratch/AG_deGasperin/fdg/:/home/fdg/data pill.simg
