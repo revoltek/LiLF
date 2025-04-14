5e7a34c0ea6b941fed2f5705746453e15ee6da43
Henrik Edler <henrik.edler@hs.uni-hamburg.de>
2025-04-14 09:43:47 +0200

    maxThreads -> maxProcs

--- pipelines/LOFAR_ddparallel.py
+++ pipelines/LOFAR_ddparallel.py
@@ -161,23 +161,23 @@ def solve_iono(MSs, c, tc, model_columns, smMHz, solint, solmode, resetant=None,
         solutions_per_direction = (solutions_per_direction/np.min(solutions_per_direction)).astype(int)
 
     if len(model_columns) >= 30 and solint > 60:
-        logger.warning('Detected many directions - limit number of parallel DP3 Threads to 1.')
-        maxThreads = 1
+        logger.warning('Detected many directions - limit number of parallel DP3 processes to 1.')
+        maxProcs = 1
     elif len(model_columns) >= 18 and solint > 60:
-        logger.warning('Detected many directions - limit number of parallel DP3 Threads to 4.')
-        maxThreads = 4
+        logger.warning('Detected many directions - limit number of parallel DP3 processes to 4.')
+        maxProcs = 4
     else:
-        maxThreads = None
+        maxProcs = None
 
     if solmode == 'phase':
         MSs.run(f'DP3 {parset_dir}/DP3-soldd.parset msin=$pathMS sol.h5parm=$pathMS/tec{tc}.h5 sol.solint={solint} \
                   sol.mode=scalarphase sol.smoothnessconstraint={smMHz}e6 sol.smoothnessreffrequency=54e6 sol.nchan=1 {antennaconstraint} \
                   sol.modeldatacolumns="[{",".join(model_columns)}]" sol.solutions_per_direction="{np.array2string(solutions_per_direction, separator=",")}"',
-                log='$nameMS_solTEC-c'+str(c)+'.log', commandType='DP3', maxProcs=maxThreads)
+                log='$nameMS_solTEC-c'+str(c)+'.log', commandType='DP3', maxProcs=maxProcs)
     else:
         MSs.run(f'DP3 {parset_dir}/DP3-solTEC.parset msin=$pathMS sol.h5parm=$pathMS/tec{tc}.h5 sol.solint={solint} {antennaconstraint} \
                   sol.modeldatacolumns="[{",".join(model_columns)}]" sol.mode={solmode}',
-                log='$nameMS_solTEC-c'+str(c)+'.log', commandType='DP3', maxProcs=maxThreads)
+                log='$nameMS_solTEC-c'+str(c)+'.log', commandType='DP3', maxProcs=maxProcs)
 
     lib_util.run_losoto(s, f'tec{tc}-c{c}', [ms+f'/tec{tc}.h5' for ms in MSs.getListStr()], losoto_parsets, 
                         plots_dir=f'{plot_dir}/plots-tec{tc}-c{c}', h5_dir=sol_dir)
