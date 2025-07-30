82d39427753b0088bf9189559ac0a2eebc2e7a09
Henrik Edler <henrik.edler@hs.uni-hamburg.de>
2025-07-30 09:45:23 +0200

    update

--- pipelines/LOFAR_ddserial.py
+++ pipelines/LOFAR_ddserial.py
@@ -1066,7 +1066,7 @@ with w.if_todo('output-vstokes-leakcal'):
    logger.info('Cleaning (lres I+V leakage-corrected)...')
    lib_util.run_wsclean(s, 'wscleanV-c'+str(cmaj)+'.log', MSs_lres.getStrWsclean(), concat_mss=True, name=imagenameV, data_column='CORRECTED_DATA', size=int(imgsizepix/4), scale=str(pixscale*4)+'arcsec',
                taper_gaussian='60arcsec', weight='briggs 0', niter=100000, gridder='wgridder', parallel_gridding=32, no_update_model_required='', minuv_l=20, mgain=0.85, parallel_deconvolution=512,
-               auto_threshold=3.0, join_channels='', fit_spectral_pol=3, channels_out=str(ch_out), deconvolution_channels=3,
+               auto_threshold=2.0, auto_mask=3.0, join_channels='', fit_spectral_pol=3, channels_out=str(ch_out), deconvolution_channels=3,
                pol='IQUV', join_polarizations = '', apply_facet_beam='', facet_beam_update=120, use_differential_lofar_beam='', facet_regions=facetregname,
                apply_facet_solutions=f'{interp_h5parm} {correct_for}', local_rms='', local_rms_window=50, local_rms_strength=0.75, beam_size=60 )
    os.system('mv %s-MFS-I-image*.fits %s-MFS-I-residual.fits ddserial/c%02i/images' % (imagenameV, imagenameV, cmaj))
