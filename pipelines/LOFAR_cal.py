6776caf2ce40c22d5cbb8e4dab65f14980121bff
JortBox <boxelaar@strw.leidenuniv.nl>
2025-08-26 09:42:21 +0200

    parset update

--- pipelines/LOFAR_cal.py
+++ pipelines/LOFAR_cal.py
@@ -29,7 +29,7 @@ less_aggressive_flag = parset.getboolean('LOFAR_cal', 'less_aggressive_flag') # 
 develop = parset.getboolean('LOFAR_cal', 'develop') # for development, don't delete files
 use_shm = parset.getboolean('LOFAR_cal', 'use_shm') # use shared memory for wsclean
 bl2flag = parset.get('flag', 'stations')
-use_GNSS = True# parset.getboolean('LOFAR_cal', 'use_GNSS') # Use GNSS for pre-TEC and FR
+use_GNSS = parset.getboolean('LOFAR_cal', 'use_GNSS') # Use GNSS for pre-TEC and FR
 #############################################################
 
 def debug_imaging(MSs, suffix, column='CORRECTED_DATA'):
