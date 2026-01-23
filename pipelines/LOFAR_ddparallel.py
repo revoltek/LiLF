fc0e1b342bae05ebd48013b46df69d2d8466b1e9
Christian Groeneveld <groeneveld@strw.leidenuniv.nl>
2026-01-23 15:45:36 +0100

    fixed some bugs due to potentially running 3 ddp cycles

--- pipelines/LOFAR_ddparallel.py
+++ pipelines/LOFAR_ddparallel.py
@@ -413,7 +413,7 @@ for c in range(maxIter):
         # TODO we need some logic here to avoid picking up very extended sources.
         patch_fluxes = sm.getColValues('I', aggregate='sum', applyBeam=True)
         # disbale 3cremoval if bright source in the field and set max_facets to 1
-        if c==0 and (patch_fluxes/si_factor > 100).any():
+        if c==0 and (patch_fluxes/si_factor > 50).any():
             logger.warning(f'Found patch with flux > 100 Jy: {patch_fluxes[patch_fluxes > 100]}, turning off 3c removal and forcing max_facets=1.')
             remove3c = False
             min_facets[0] = 1
@@ -824,8 +824,13 @@ for c in range(maxIter):
             MSs.run(f'DP3 {parset_dir}/DP3-cor.parset msin=$pathMS msin.datacolumn=MODEL_DATA msout.datacolumn=MODEL_DATA \
                     cor.parmdb={sol_dir}/cal-tec-sf-c' + str(c-1) + '.h5 cor.correction=phase000 cor.invert=False',
                     log='$nameMS_sidelobe_corrupt.log', commandType='DP3')
-            # Corrupt MSs:MODEL_DATA -> MODEL_DATA (sf corr, dieamp corr)
-            corr_die_amp(MSs, col='MODEL_DATA', fulljones=fulljones, invert=False)
+            if bright_source:
+                MSs.run(f'DP3 {parset_dir}/DP3-cor.parset msin=$pathMS msin.datacolumn=MODEL_DATA msout.datacolumn=MODEL_DATA \
+                        cor.parmdb={sol_dir}/cal-subfield_ampl-c' + str(0) + '.h5 cor.correction=amplitudeNorm cor.invert=False',
+                        log='$nameMS_sidelobe_corrupt.log', commandType='DP3')
+            if (c == 1 and not bright_source):
+                # Corrupt MSs:MODEL_DATA -> MODEL_DATA (sf corr, dieamp corr)
+                corr_die_amp(MSs, col='MODEL_DATA', fulljones=fulljones, invert=False)
 
         # subtract external region MSs: PREPARED_DATA - MODEL_DATA -> SUBFIELD_DATA
         logger.info('Subtracting external region model (SUBFIELD_DATA = PREPARED_DATA - MODEL_DATA)...')
